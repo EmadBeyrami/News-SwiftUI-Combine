4f17d282e04148c101dc6ddc510fc0543b713e74
Emad Beyrami <emad.beyrami#>
2021-07-17 00:04:35 +0430

    Initial Commit

---
+++ News SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News SwiftUI
+//
+//  Created by Emad Bayramy on 7/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
